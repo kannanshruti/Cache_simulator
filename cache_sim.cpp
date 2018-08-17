385f2334fb6d93548b569795fc274a11553a0b8a
kshruti <kshruti@bu.edu>
2018-08-17 11:56:16 -0400

    Added a class structure

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,11 +1,21 @@
 /*
-Description: Cache simulator
+
+Name: Cache memory simulator
+
+Description: Code which simulates the working of a cache memory. The command
+line arguments for the cache include the cache size and its associativity.
+The code also accepts an input file, with each line containing a memory 
+address and a boolean value representing 'Load' or 'Store' operation at 
+that address. The output of the program is a count of the number of memory
+accesses, 'Load's, 'Store's and corresponding 'Hits' and 'Misses'.
+
+Authors: Shruti Kannan, Shreyas Joshi
 */
-#include "stdafx.h"
+
 #include <stdio.h>
-#include <tchar.h>
+#include <string>
 #include <iostream> 
-#include <string> 
+#include <string.h>
 #include <fstream>
 #include <vector>
 #include <sstream>
@@ -13,14 +23,15 @@ Description: Cache simulator
 
 using namespace std;
 
-// grouping all the variables related to the input memory 
-typedef struct cacheLine { 
+// Grouping all the variables related to the input memory 
+typedef struct memory_addr { 
 	unsigned int tag; // No. of tag bits
 	unsigned int index; // No. of index bits
 	unsigned int offset; // No. of offset bits
 	int addr_offset; // Integer representation of offset
 	int addr_index; // Integer representation of index
 	string addr_tag; // Tag is string (binary)
+	int ld_st;
 } struct1;
 
 // Grouping all the variables related to the whole cache
@@ -31,64 +42,103 @@ typedef struct cache {
 	bool nmru; // Not Most Recently Used bit
 } struct2;
 
-string hex_to_bin(char hex[]); 
-void access_extract(string mem_addr, struct1 &mem_access);
-int bin_to_dec(string bin);
-void set_nmru(vector <struct2> &cache_set, int curr_way, unsigned int nWayAssociativity);
 
-int main(int argc, char* argv[]) {
-	unsigned int cacheBlockSize, nCacheBlocks, nCacheSets, nWayAssociativity;
-	char nCurrentAccess[15];
-	unsigned int offset_bits, index_bits, tag_bits;
-	struct1 mem_access;
-	int hit = 0, store_hit = 0, load_hit = 0, miss = 0,  store_miss = 0, load_miss = 0;
-	int count_ways = 0, count_evict = 0, evict = 0;
-	int nLoad = 0, nStore = 0, nAccesses = 0;
-	int ld_st = 0;	
-	cacheBlockSize = 128;
-	offset_bits = 7;
-
-	// File access and looping through the file contents
-	string traffic_file = argv[1]; 
-	int cacheSize = stod(argv[2]);
-	nWayAssociativity = stoi(argv[3]);
-	ifstream ifs(traffic_file);
-	string line, line1;
+class cache_sim {
+	/*
+		Description: Implements the methods required to simulate cache memory
+	*/
+public:
 	vector<string> input_address;
 	vector<int> input_ldst;	
-	while (getline(ifs, line)) { // Looping through each line of the input text file
-		stringstream ss(line);
-		int tmp = 0;
-		while (getline(ss, line1, ' ')) { // Extracting address and load/store bit from each line
-			if (tmp % 2 == 0) {
-				input_address.push_back(line1);
-			}
-			else if (tmp % 2 != 0) {
-				input_ldst.push_back(stoi(line1));
-				if (line1 == "0")
-					++nLoad;
-				else
-					++nStore;
-			}
-			tmp += 1;
-		}
-	}
-	nAccesses = input_address.size();
+	struct1 mem_access;
+	int nLoad = 0, nStore = 0, nAccesses = 0;
+	int hit = 0, store_hit = 0, load_hit = 0;
+	int miss = 0,  store_miss = 0, load_miss = 0;
+	int count_ways = 0, count_evict = 0, nEvict = 0;
+	unsigned int cacheBlockSize;
 
-	cout << "Input: \n";
-	for (int i = 0; i < input_ldst.size(); i++) {
-		cout << input_address[i] << " " << input_ldst[i] << "\n";
-	}
+	cache_sim(string traffic_file, int cacheSize, unsigned int nWayAsso);
+	void get_file_line(string input_address, int input_ldst);
+	void check_in_cache();
+	void check_cache_full();
+	void check_cache_eviction();
+	bool not_found_in_cache();
+	bool is_cache_eviction_necessary();
+private:
+	unsigned int offset_bits, index_bits, tag_bits;
+	unsigned int nWayAssociativity, nCacheBlocks, nCacheSets;
+	vector <vector <struct2>> cache;
+
+	void get_file_contents(string traffic_file);
+	void access_extract(string mem_addr, struct1 &mem_access);
+	void create_cache(vector <vector <struct2>> &cache, unsigned intnCacheSets,
+					  unsigned int nWayAssociativity);
+	string hex_to_bin(char hex[]);
+	int bin_to_dec(string bin);
+	void set_nmru(vector <struct2> &cache_set, int curr_way, unsigned int nWayAssociativity);
+};
+
+cache_sim::cache_sim(string traffic_file, int cacheSize,
+						  unsigned int nWayAsso) {
+	cacheBlockSize = 128;
+	offset_bits = 7;
+	nWayAssociativity = nWayAsso;
+
+	// Reading the input file
+	get_file_contents(traffic_file);
 
 	// Calculating cache parameters
 	nCacheBlocks = cacheSize / cacheBlockSize;
 	nCacheSets = nCacheBlocks / nWayAssociativity;
 	index_bits = log2(nCacheSets);
-	tag_bits = 32 - index_bits - offset_bits; // Given: 32 bit memory address
+	tag_bits = 32 - index_bits - offset_bits;
 
-	// Creating a cache 2D Vector (default settings)
-	vector <vector <struct2>> cache(nCacheSets, vector<struct2>(nWayAssociativity)); 
-	for (unsigned int i = 0; i < nCacheSets; i++) {
+	// Initializing the cache
+	cache.resize(nCacheSets, vector<struct2>(nWayAssociativity));
+	create_cache(cache, nCacheSets, nWayAssociativity);
+
+	// Set the number of bits
+	mem_access.tag = tag_bits;
+	mem_access.index = index_bits;
+	mem_access.offset = offset_bits;
+}
+
+
+void cache_sim::get_file_contents(string traffic_file) {
+	/*	Description: Reads the contents of "traffic_file". Stores the
+		memory addresses in "input_address", and the load/store status
+		in "input_ldst". The number of required accesses is stored 
+		as "nAccesses"
+	*/
+	ifstream ifs(traffic_file);
+	string line, line1;
+	// Looping through each line of the input text file
+	while (getline(ifs, line)) {
+		stringstream ss(line);
+		int tmp = 0;
+		// Extracting address and load/store bit from each line
+		while (getline(ss, line1, ' ')) {
+			if (tmp % 2 == 0) {
+				input_address.push_back(line1);
+			}
+			else if (tmp % 2 != 0) {
+				input_ldst.push_back(stoi(line1));
+				if (line1 == "0") ++nLoad;
+				else ++nStore;
+			}
+			tmp += 1;
+		}
+	}
+	nAccesses = input_address.size();
+	return;
+}
+
+void cache_sim::create_cache(vector <vector <struct2>> &cache, unsigned int nCacheSets,
+				  unsigned int nWayAssociativity) {
+	/*	Description: Initializes an empty cache based on the 
+		number of sets and the associativity
+	*/
+	for (unsigned int i = 0; i < nCacheSets; i++) { // add to init
 		for (unsigned int j = 0; j < nWayAssociativity; j++) {
 			cache[i][j].valid = false;
 			cache[i][j].addr_index = i;
@@ -96,101 +146,99 @@ int main(int argc, char* argv[]) {
 			cache[i][j].nmru = false;
 		}
 	}
-	
-	// Loop over file contents
-	for (int index = 0; index < nAccesses; index++) {
-
-		// Decoding the input memory address
-		strcpy_s(nCurrentAccess, input_address[index].c_str());
-		string mem_addr = hex_to_bin(nCurrentAccess); // Converting Hex address to a binary string
-		ld_st = input_ldst[index];
-		cout << "Memory address: " << mem_addr << " Load/Store: " << ld_st << "\n";
-
-		mem_access.tag = tag_bits;
-		mem_access.index = index_bits;
-		mem_access.offset = offset_bits;
-		access_extract(mem_addr, mem_access); // Separating index, offset, tag of the input address
-		cout << " Tag " << mem_access.addr_tag << "\n";
-		cout << " Index " << mem_access.addr_index << "\n";
-		cout << " Offset " << mem_access.addr_offset <<"\n";
-
-		// Tag and valid bit match to find the hit conditions
-		for (unsigned int j = 0; j < nWayAssociativity; j++) {
-			if ((cache[mem_access.addr_index][j].tag == mem_access.addr_tag) && (cache[mem_access.addr_index][j].valid == true)) { // Found in cache
-				set_nmru(cache[mem_access.addr_index], j, nWayAssociativity);
-				if (ld_st == 0) {
-					++load_hit;
-				}
-				else {
-					++store_hit;
-				}
-				++hit;
-				count_ways = 0;
-				break;
-			}
-			else { // Not found in cache
-				++count_ways;			
-			}
-		}
-
-		if (count_ways == nWayAssociativity) {
-			for (int i = 0; i < nWayAssociativity; i++) {
-				if (cache[mem_access.addr_index][i].valid == false) { // If not found in cache + that index is not full
-					cache[mem_access.addr_index][i].tag = mem_access.addr_tag;
-					cache[mem_access.addr_index][i].valid = true;
-					set_nmru(cache[mem_access.addr_index], i, nWayAssociativity);
-					count_evict = 0;
-					count_ways = 0;
-					if (ld_st == 0) {
-						++load_miss;
-					}
-					else {
-						++store_miss;
-					}
-					++miss;
-					break;
-				}
-				else { 
-					++count_evict;
-				}
-			}
-		}
-		
-		if (count_evict == nWayAssociativity) { // Not found in the cache and cache is full
-			for (int i = 0; i < nWayAssociativity; i++) {
-				if (cache[mem_access.addr_index][i].nmru == false) { // Eviction based on NMRU 
-					cache[mem_access.addr_index][i].tag = mem_access.addr_tag;
-					cache[mem_access.addr_index][i].valid = true;
-					set_nmru(cache[mem_access.addr_index], i, nWayAssociativity);
-					++evict;
-					if (ld_st == 0) {
-						++load_miss;
-					}
-					else {
-						++store_miss;
-					}
-					++miss;
-					count_ways = 0;
-					count_evict = 0;
-					break;
-				}
-			}
-		}
-	}
-	cout << "Number of accesses:" << nAccesses << "\n";
-	cout << "Number of loads:" << nLoad << "\n";
-	cout << "Number of stores:" << nStore << "\n";
-	cout << "Number of hits:" << hit << "\n";
-	cout << "Number of load hits:" << load_hit << "\n";
-	cout << "Number of store hits:" << store_hit << "\n";
-	cout << "Number of miss:" << miss << "\n";
-	cout << "Number of load miss:" << load_miss << "\n";
-	cout << "Number of store miss:" << store_miss << "\n";
-	cout << "Number of evictions:" << evict << "\n";
-	return 0;
+	return;
 }
 
-void set_nmru(vector <struct2> &cache_set, int curr_way, unsigned int nWayAssociativity) {
+void cache_sim::get_file_line(string input_address,int input_ldst) {
+	/*	Description: Stores the memory address and load/store status
+		to the "mem_access" struct
+	*/
+	char nCurrentAccess[15];
+	strcpy(nCurrentAccess, input_address.c_str());
+	string mem_addr = hex_to_bin(nCurrentAccess); // Converting Hex address to a binary string
+	mem_access.ld_st = input_ldst;
+	access_extract(mem_addr, mem_access);
+	return;
+}
+
+bool cache_sim::not_found_in_cache() {
+	/*	Description: Returns 'True' if the memory address has been
+		searched across all the cache ways for the particular index,
+		and not found. 
+	*/
+	return count_ways == nWayAssociativity;
+}
+
+void cache_sim::check_cache_full() {
+	/*	Description: If the address index is not found in any of the cache ways,
+		checks if the cache is full , if not, the information from the new 
+		address is to be inserted into the cache. 
+	*/
+	for (int i = 0; i < nWayAssociativity; i++) {
+		if (cache[mem_access.addr_index][i].valid == false) { 
+			cache[mem_access.addr_index][i].tag = mem_access.addr_tag;
+			cache[mem_access.addr_index][i].valid = true;
+			set_nmru(cache[mem_access.addr_index], i, nWayAssociativity);
+			count_evict = 0;
+			count_ways = 0;
+			if (mem_access.ld_st == 0) ++load_miss;
+			else ++store_miss;
+			++miss;
+			break;
+		}
+		else ++count_evict;
+	}
+	return;
+}
+
+void cache_sim::check_in_cache() {
+	/*	Description: Checks if the memory address index is found in the cache.
+		If yes, a corresponding 'hit' is incremented.
+	*/
+	for (unsigned int j = 0; j < nWayAssociativity; j++) {
+		if ((cache[mem_access.addr_index][j].tag == mem_access.addr_tag) && (cache[mem_access.addr_index][j].valid == true)) { // Found in cache
+			set_nmru(cache[mem_access.addr_index], j, nWayAssociativity);
+			if (mem_access.ld_st == 0) ++load_hit;
+			else ++store_hit;
+			++hit;
+			count_ways = 0;
+			break;
+		}
+		else ++count_ways; // Not found in cache
+	}
+	return;
+}
+
+bool cache_sim::is_cache_eviction_necessary() {
+	/*	Description: Checks if an eviction from cache is required.
+		Returns 'True' if eviction is necessary
+	*/
+	return count_evict == nWayAssociativity;
+}
+
+void cache_sim::check_cache_eviction() {
+	/*	Description: Determines which cache index is to be evicted.
+		Loops over the cache ways of the index in question, and if the "nmru"
+		bit is reset, evicts that particular value.
+	*/
+	for (int i = 0; i < nWayAssociativity; i++) {
+		if (cache[mem_access.addr_index][i].nmru == false) {
+			cache[mem_access.addr_index][i].tag = mem_access.addr_tag;
+			cache[mem_access.addr_index][i].valid = true;
+			set_nmru(cache[mem_access.addr_index], i, nWayAssociativity);
+			++nEvict;
+			if (mem_access.ld_st == 0) ++load_miss;
+			else ++store_miss;
+			++miss;
+			count_ways = 0;
+			count_evict = 0;
+			break;
+		}
+	}
+	return;
+}
+
+void cache_sim::set_nmru(vector <struct2> &cache_set, int curr_way, unsigned int nWayAssociativity) {
 	/*	Description: Setting NMRU bit of the most recently used index to 1 and the others to 0
 		Input:	cache_set: Cache set which is to be 
 				curr_way:
@@ -207,38 +255,7 @@ void set_nmru(vector <struct2> &cache_set, int curr_way, unsigned int nWayAssoci
 	}
 }
 
-int bin_to_dec(string bin) {
-	/*	Description: Converts binary string to integer 
-		Input: bin: Binary string to be converted to decimal
-		Output: dec: Converted integer
-	*/
-	int bin1 = stoi(bin);
-	int rem, counter=0, dec=0;
-	while (bin1 != 0) {
-		rem = bin1 % 10;
-		dec += pow(2, counter)*rem;
-		bin1 = bin1 / 10;
-		counter++;
-	}
-	return dec;
-}
-
-void access_extract(string mem_addr, struct1 &mem_access) {
-	/*	Description: Separates the index, offset and tag bits from the input memory address
-		Input:	mem_addr: String binary address
-				mem_access: struct containing separated index, offset and tag
-	*/
-	string tmp1 = mem_addr.substr(mem_addr.length() - mem_access.offset, mem_access.offset);
-	mem_access.addr_offset = bin_to_dec(tmp1);
-
-	string tmp2 = mem_addr.substr(mem_addr.length() - mem_access.offset- mem_access.index, mem_access.index);
-	mem_access.addr_index = bin_to_dec(tmp2);
-
-	mem_access.addr_tag = mem_addr.substr(0, mem_access.tag);
-}
-
-
-string hex_to_bin(char hex[]) {
+string cache_sim::hex_to_bin(char hex[]) {
 	/*	Description: Convert hex character array to binary string
 		Input: hex: Character array containing the hex string
 		Output: bin: Converted binary string
@@ -269,4 +286,84 @@ string hex_to_bin(char hex[]) {
 		}
 	}
 	return bin;
+}
+
+void cache_sim::access_extract(string mem_addr, struct1 &mem_access) {
+	/*	Description: Separates the index, offset and tag bits from the input memory address
+		Input:	mem_addr: String binary address
+				mem_access: struct containing separated index, offset and tag
+	*/
+	string tmp1 = mem_addr.substr(mem_addr.length() - mem_access.offset, mem_access.offset);
+	mem_access.addr_offset = bin_to_dec(tmp1);
+
+	string tmp2 = mem_addr.substr(mem_addr.length() - mem_access.offset- mem_access.index, mem_access.index);
+	mem_access.addr_index = bin_to_dec(tmp2);
+
+	mem_access.addr_tag = mem_addr.substr(0, mem_access.tag);
+
+	return;
+}
+
+int cache_sim::bin_to_dec(string bin) {
+	/*	Description: Converts binary string to integer 
+		Input: "bin": Binary string to be converted to decimal
+		Output: "dec": Converted integer
+	*/
+	int bin1 = stoi(bin);
+	int rem, counter=0, dec=0;
+	while (bin1 != 0) {
+		rem = bin1 % 10;
+		dec += pow(2, counter)*rem;
+		bin1 = bin1 / 10;
+		counter++;
+	}
+	return dec;
+}
+
+
+int main(int argc, char* argv[]) {
+	string traffic_file = argv[1]; 
+	int cacheSize = stod(argv[2]);
+	unsigned int nWayAssociativity = stoi(argv[3]);
+	
+	cache_sim c1(traffic_file, cacheSize, nWayAssociativity);
+
+	cout << "Input: \n";
+	for (int i = 0; i < c1.input_ldst.size(); i++) {
+		cout << c1.input_address[i] << " " << c1.input_ldst[i] << "\n";
+	}
+
+	// Loop over file contents
+	for (int index = 0; index < c1.nAccesses; index++) {
+		// Decoding the input memory address and
+		// Separating index, offset, tag of the input address
+		c1.get_file_line(c1.input_address[index], c1.input_ldst[index]);
+		cout << " Tag " << c1.mem_access.addr_tag << "\n";
+		cout << " Index " << c1.mem_access.addr_index << "\n";
+		cout << " Offset " << c1.mem_access.addr_offset <<"\n";
+		cout << " LDST " << c1.mem_access.ld_st <<"\n";
+
+		// Check if present in cache
+		c1.check_in_cache();
+
+		// If not found in cache and cache not full, insert to cache
+		if (c1.not_found_in_cache())	{	
+			c1.check_cache_full();
+		}
+		// If not found in cache and cache full, evict one entry from cache
+		if (c1.is_cache_eviction_necessary()) { 
+			c1.check_cache_eviction();
+		}
+	}
+	cout << "Number of accesses:" << c1.nAccesses << "\n";
+	cout << "Number of loads:" << c1.nLoad << "\n";
+	cout << "Number of stores:" << c1.nStore << "\n";
+	cout << "Number of hits:" << c1.hit << "\n";
+	cout << "Number of load hits:" << c1.load_hit << "\n";
+	cout << "Number of store hits:" << c1.store_hit << "\n";
+	cout << "Number of miss:" << c1.miss << "\n";
+	cout << "Number of load miss:" << c1.load_miss << "\n";
+	cout << "Number of store miss:" << c1.store_miss << "\n";
+	cout << "Number of evictions:" << c1.nEvict << "\n";
+	return 0;
 }
